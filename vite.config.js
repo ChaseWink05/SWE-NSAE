0bbbbb073ba1665930325651603e6bc6308d0fe6
ChaseWink05 <155601183+ChaseWink05@users.noreply.github.com>
2025-03-17 17:53:36 -0400

    pudsh

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/SWE-NSAE/',  // Set this to your repository name (case-sensitive)
+  base: '/home/',  // Set this to your repository name (case-sensitive)
 });
